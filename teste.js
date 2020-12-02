dea204deb48347ae6e9e4f491c69a2e49fdb9d63
Gamer33d <dalbenig@gmail.com>
2020-12-02 16:17:37 -0300

    sequelize

--- teste.js
+++ teste.js
@@ -4,3 +4,49 @@ const sequelize = new Sequelize('test', 'root', 'gui010606', {
     dialect: "mysql"
 
 });
+//Definindo o model
+const Postagem = sequelize.define('postagens',{
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    },
+
+})
+const Usuario = sequelize.define('Usuarios',{
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+
+    email: {
+        type: Sequelize.STRING
+    }
+
+})
+
+/*Inserta dados na tabela
+Postagem.create({
+    titulo: "Qualquer titulo",
+    conteudo: "ahdajdhajskhdasjdh"
+})
+
+Usuario.create({
+    nome: "Gamer33d",
+    sobrenome: "Gamer34d",
+    idade: 12,
+    email: "blabla@mail.com"
+})
+*/
+
+
+
+
+//Criando a table
+//Usuario.sync({force: true})
